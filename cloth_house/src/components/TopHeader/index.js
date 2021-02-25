aff8e44d54c605e6a90cc161ac41422839075847
shashank gutha <shashankgutha@gmail.com>
2021-02-25 11:26:35 +0530

    add header component with logo, searchbar and icons for the app

--- cloth_house/src/components/TopHeader/index.js
+++ cloth_house/src/components/TopHeader/index.js
@@ -1,28 +1,41 @@
 import React from "react";
 import ErrorBoundary from "../../Hoc/ErrorBoundary.js";
 import { Layout } from "antd";
-import { Input, Row, Col  } from "antd";
-import { AudioOutlined,SearchOutlined } from '@ant-design/icons';
+import { Input, Row, Col, Select,Space,Button  } from "antd";
+import { AudioOutlined,SearchOutlined,EnvironmentOutlined ,GlobalOutlined } from '@ant-design/icons';
 import "./topHeader.css";
 const { Header, Footer, Sider, Content } = Layout;
-
+const { Option } = Select;
 const SmileBagHeader= () =>{
   return (
     <>
     <ErrorBoundary>
       <Header className="header">
-        <Row align="start">
-          <Col  span={6}>
+        <Row>
+        <Space align="center">
         <div className="logo">
           SmileBag{" "}
           <img className="logoImage" src="./assets/images/Raster.png"></img>
         </div>
-        </Col >
-        <Col ol span={6}>
+       
         <div className="searchInput">
         <Input size="large" placeholder="Search" suffix={<SearchOutlined onClick={() => {}} />} />
         </div>
-        </Col >
+        <div >
+        <Select className="selectpin" suffixIcon={<EnvironmentOutlined className="selectpin"/>} size="large" bordered={false} defaultValue="500070">
+        <Option value="500070">500070</Option>
+        <Option value="500080">500080</Option>
+        </Select>
+        </div>
+        <div className="login">
+        <Button className="login-button" block={true} type="primary" ghost={true} size="large">
+          Login/Signup
+        </Button>
+        </div>
+        <div className="globe">
+        <GlobalOutlined onClick={() => {}} className="globe"/>
+        </div>
+        </Space>
         </Row>
       </Header>
       </ErrorBoundary>
