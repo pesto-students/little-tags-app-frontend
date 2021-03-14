06ed48098243883a39045f8866b0315c913cd746
Mynk11 <mayank@zonkafeedback.com>
2021-03-14 11:02:19 +0530

    added test cases for cart

--- cloth_house/cypress/config.js
+++ cloth_house/cypress/config.js
@@ -1,5 +1,4 @@
-export const testUrl="http://localhost:3000"
-//"https://quizzical-nobel-71a09b.netlify.app/";
+export const testUrl="https://quizzical-nobel-71a09b.netlify.app/";
 export  const data = {
     email: 'user@email.com',
     password: '123456',
